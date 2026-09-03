71b17fe63db190843100b899c64cde4d2374d2e6
adamiturabi <adamiturabi@adamiturabi-MacBook-Pro.local>
2026-09-03 07:35:51 -0500

    converted to quarto

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/bash
+#rm -rf content/*
+#touch content/refs.qmd
+#cp srcqmd/*.qmd content
+#quarto render --profile makepdf
+#quarto render --profile makehtml
+#mv pdf_out_dir/*.pdf docs/
+#rmdir pdf_out_dir
+quarto render
+
